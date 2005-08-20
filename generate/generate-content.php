1522362256b64ccdf6767954e93b8e1d2b8c5f63
Gerhard Killesreiter <killes_www_drop_org@227.no-reply.drupal.org>
2005-08-20 13:51:47 +0000

    updateing for cvs, added longer time out.

--- generate/generate-content.php
+++ generate/generate-content.php
@@ -1,7 +1,11 @@
 <?php
 // $Id$
 include_once "includes/bootstrap.inc";
-include_once "includes/common.inc";
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+// If not in 'safe mode', increase the maximum execution time:
+if (!ini_get('safe_mode')) {
+  set_time_limit(240);
+}
 
 function create_nodes($records, $users) {
 
