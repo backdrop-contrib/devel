cf9cd64b82246974e4bef82771a02de42ed5bd3d
Bèr Kessels <ber@webschuur.com>
2006-01-10 20:42:09 +0000

    * changed the database query to the latest database structure * added a BOOTSTRAP_FULL to load all correct files

--- generate/generate-users.php
+++ generate/generate-users.php
@@ -3,7 +3,8 @@
    * This script generates users for testing purposes.
    */
 include_once 'includes/bootstrap.inc';
-include_once 'includes/common.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
 
 // use one of your own domains. Using somebody else's domain is rude.
 $domain = '';
@@ -17,7 +18,7 @@ function make_users($num, $domain) {
     $name = md5($i);
     $mail = $name .'@'. $domain;
     $status = 1;
-    db_query("INSERT INTO {users} (uid, name, mail, status, created, changed) VALUES (%d, '%s', '%s', %d, %d, %d)", $uid, $name, $mail, $status, time(), time());
+    db_query("INSERT INTO {users} (uid, name, mail, status, created, access) VALUES (%d, '%s', '%s', %d, %d, %d)", $uid, $name, $mail, $status, time(), time());
   }
   db_query("UPDATE {sequences} SET id = %d WHERE name = 'users_uid'", $uid);
 }
