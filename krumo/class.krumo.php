4b6fca53a925566e09cdd789f05826c5cde12bac
Dave Reid <dave@davereid.net>
2009-09-23 18:13:19 +0000

    #354154 by jonathan1055, Dave Reid: Fixed path to files in Krumo CSS.

--- krumo/class.krumo.php
+++ krumo/class.krumo.php
@@ -665,7 +665,7 @@ This is a list of all the values from the <code><b><?php echo realpath($ini_file
 			//
 			
 			// DEVEL: changed for Drupal path system.
-			$css_url = url(drupal_get_path('module', 'devel')) . "/krumo/skins/{$skin}/";
+			$css_url = file_create_url(drupal_get_path('module', 'devel') . "/krumo/skins/{$skin}/");
 			
 			$css = preg_replace('~%url%~Uis', $css_url, $css);
 			
