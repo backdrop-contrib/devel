924d243c46b0c45bea9bc3872e523ad461eafc52
Gregory Netsas <gregory@userfriendly.tech>
2019-05-30 04:12:59 +1000

    Update class.krumo.php
    https://git.drupalcode.org/project/devel/commit/bd4061f

--- lib/krumo/class.krumo.php
+++ lib/krumo/class.krumo.php
@@ -1047,7 +1047,7 @@ This is a list of all the values from the <code><b><?php echo realpath($ini_file
 ?>
 <li class="krumo-child">
 
-  <div class="krumo-element<?php echo count($data) > 0 ? ' krumo-expand' : '';?>"
+  <div class="krumo-element<?php echo !empty($data) ? ' krumo-expand' : '';?>">
     <?php if (count($data) > 0) {?> onClick="krumo.toggle(this);"<?php } ?>
     onMouseOver="krumo.over(this);"
     onMouseOut="krumo.out(this);">
@@ -1099,7 +1099,7 @@ This is a list of all the values from the <code><b><?php echo realpath($ini_file
 ?>
 <li class="krumo-child">
 
-  <div class="krumo-element<?php echo count($data) > 0 ? ' krumo-expand' : '';?>"
+  <div class="krumo-element<?php echo ' krumo-expand';?>">
     <?php if (count($data) > 0) {?> onClick="krumo.toggle(this);"<?php } ?>
     onMouseOver="krumo.over(this);"
     onMouseOut="krumo.out(this);">
@@ -1110,7 +1110,7 @@ This is a list of all the values from the <code><b><?php echo realpath($ini_file
       <strong class="krumo-class"><?php echo get_class($data);?></strong>
   </div>
 
-  <?php if (count($data)) {
+  <?php {
     krumo::_vars($data);
     } ?>
 </li>
